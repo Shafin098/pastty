7a11880c3c7615e3016fc7c544c482a3da14f97d
Shafin <shafinashraf074@gmail.com>
2021-01-02 19:32:06 +0600

    Try to fix firewall related problem

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
-const clippy = require('clipboardy')
-const express = require('express')
-const qrcode = require('qrcode-terminal')
-const getPort = require('get-port')
 const dns = require('dns')
 const os = require('os')
 const path = require('path')
-const { start } = require('repl')
+const { exec } = require('child_process')
+
+const clippy = require('clipboardy')
+const qrcode = require('qrcode-terminal')
+const getPort = require('get-port')
+
+const express = require('express')
 
 const app = express();
 
@@ -17,6 +19,23 @@ app.get('/paste', (req, res) => {
     clippy.writeSync(pasteFromMobile)
     res.redirect('/')
 })
+function removeFirewallOnPort(port) {
+    const ALLOW_CONNECTION_ON_PORT_COMMAND = `netsh advfirewall firewall add rule name=\\\`"Allow connection on TCP Port ${port}\\\`" dir=in action=allow protocol=TCP localport=${port}`
+    // dont split first argument of exec in multiple line it will cause issue
+    const commad1 = `powershell -Command "Start-Process -Wait -FilePath cmd -Verb RunAs -ArgumentList {/k ${ALLOW_CONNECTION_ON_PORT_COMMAND}}"`
+    exec(commad1, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`)
+            return
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`)
+            return
+        }
+        console.log(`stdout: ${stdout}`)
+        console.log(commad1);
+    })
+}
 
 async function startServer() {
     const PORT = await getPort()
@@ -26,6 +45,7 @@ async function startServer() {
             qrcode.generate(URL, { small: true })
             console.log(`Scan qrcode or open link below`)
             console.log(URL)
+            removeFirewallOnPort(PORT)
         })
     })
 }
