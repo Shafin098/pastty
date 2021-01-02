cebf2699423e8791ae135e5f36598392ea0c38d7
Shafin <shafinashraf074@gmail.com>
2021-01-02 21:37:18 +0600

    Fix firewall related issue

--- index.js
+++ index.js
@@ -19,25 +19,41 @@ app.get('/paste', (req, res) => {
     clippy.writeSync(pasteFromMobile)
     res.redirect('/')
 })
-function removeFirewallOnPort(port) {
-    const ALLOW_CONNECTION_ON_PORT_COMMAND = `netsh advfirewall firewall add rule name=\\\`"Allow connection on TCP Port ${port}\\\`" dir=in action=allow protocol=TCP localport=${port}`
-    // dont split first argument of exec in multiple line it will cause issue
-    const commad1 = `powershell -Command "Start-Process -Wait -FilePath cmd -Verb RunAs -ArgumentList {/k ${ALLOW_CONNECTION_ON_PORT_COMMAND}}"`
-    exec(commad1, (error, stdout, stderr) => {
+
+function runCommandOnCmd(command) {
+    // Elevates cmd privilege then executes command
+    // /c closes cmd window and /k keeps window
+    const BYPASS_FIREWALL = `powershell -Command "Start-Process -Wait -FilePath cmd -Verb RunAs -ArgumentList {/c ${command}}"` // dont split this line ever
+    exec(BYPASS_FIREWALL, (error, stdout, stderr) => {
         if (error) {
-            console.log(`error: ${error.message}`)
-            return
+            console.error(`Error: ${error.message}`)
+            process.exit(1)
         }
         if (stderr) {
-            console.log(`stderr: ${stderr}`)
-            return
+            console.error(`Error: ${stderr}`)
+            process.exit(1)
         }
-        console.log(`stdout: ${stdout}`)
-        console.log(commad1);
     })
 }
 
-async function startServer() {
+// Adds rule to firewall inbound to allow connection on specific port
+function removeFirewallOnPort(port) {
+    const ALLOW_CONNECTION_ON_PORT_COMMAND = `netsh advfirewall firewall add rule name="Pastty" dir=in action=allow protocol=TCP localport=${port}`
+    runCommandOnCmd(ALLOW_CONNECTION_ON_PORT_COMMAND)
+}
+
+// Remove previously created rule to allow connection on specific port
+function resetFirewallToPreviousState(port) {
+    const BLOCK_CONNECTION_ON_PORT_COMMAND = `netsh advfirewall firewall delete rule name="Pastty" dir=in action=allow protocol=TCP localport=${port}`
+    runCommandOnCmd(BLOCK_CONNECTION_ON_PORT_COMMAND)
+}
+
+function killServer(port) {
+    resetFirewallToPreviousState(port)
+    app.close(() => "Closing pastty server")
+}
+
+async function startServer(app) {
     const PORT = await getPort()
     app.listen(PORT, () => {
         dns.lookup(os.hostname(), (err, add, fam) => {
@@ -48,6 +64,9 @@ async function startServer() {
             removeFirewallOnPort(PORT)
         })
     })
+
+    process.on('SIGINT', () => killServer(PORT))
+    process.on('SIGTERM', () => killServer(PORT))
 }
 
-startServer()
+startServer(app)
