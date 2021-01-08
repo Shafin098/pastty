cb4261a9366e928238a8d3491dabb5dd9b2c8cc1
Shafin <shafinashraf074@gmail.com>
2021-01-08 16:29:54 +0600

    Add platform check before running platform specifc code

--- index.js
+++ index.js
@@ -38,12 +38,18 @@ function runCommandOnCmd(command) {
 
 // Adds rule to firewall inbound to allow connection on specific port
 function removeFirewallOnPort(port) {
+    if (process.platform != 'win32') {
+        return
+    }
     const ALLOW_CONNECTION_ON_PORT_COMMAND = `netsh advfirewall firewall add rule name="Pastty" dir=in action=allow protocol=TCP localport=${port}`
     runCommandOnCmd(ALLOW_CONNECTION_ON_PORT_COMMAND)
 }
 
 // Remove previously created rule to allow connection on specific port
 function resetFirewallToPreviousState(port) {
+    if (process.platform != 'win32') {
+        return
+    }
     const BLOCK_CONNECTION_ON_PORT_COMMAND = `netsh advfirewall firewall delete rule name="Pastty" dir=in action=allow protocol=TCP localport=${port}`
     runCommandOnCmd(BLOCK_CONNECTION_ON_PORT_COMMAND)
 }
